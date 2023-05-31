5946bddf4598a78f1f1d107b99b3600b04224036
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-30 20:04:48 -0500

    pom.xml updated added lambok dependency and new model class student created

---
+++ Java-Servlet-Jdbc-Student-with-webxml/src/main/java/com/model/Student.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int S_ID;
+    private int S_AGE;
+    private String S_FNAME;
+    private String S_LNAME;
+    private String S_GENDER;
+    private String S_BRANCH;
+}
