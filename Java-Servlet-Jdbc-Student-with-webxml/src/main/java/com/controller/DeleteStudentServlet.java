5303dcba2a031f37787967320aa6c3929e689635
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-06-01 19:50:25 -0500

    delete student servlet added

---
+++ Java-Servlet-Jdbc-Student-with-webxml/src/main/java/com/controller/DeleteStudentServlet.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import com.dao.StudentDao;
+import com.db.DBConnect;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class DeleteStudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int deleteStudent = Integer.parseInt(req.getParameter("id"));
+        System.out.println(deleteStudent);
+        StudentDao studentDao = new StudentDao(DBConnect.getConnection());
+        boolean flag = studentDao.deleteStudent(deleteStudent);
+        if(flag) {
+            System.out.println("DELETED");
+        } else {
+            System.out.println("ISSUE");
+        }
+        resp.sendRedirect("displayStudents");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("displayStudents");
+        requestDispatcher.forward(req, resp);
+    }
+}
